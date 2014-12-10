c2968a0df52b77b71cc9869b943c2f8a2b8f94e3
SmileYun <zhuyunsheng23@gmail.com>
2014-12-10 22:18:26 +0800

    hi 晚上，现在不报错了.. Signed-off-by: SmileYun <zhuyunsheng23@gmail.com>

--- src/com/cqupt/ui/widget/DynamicView.java
+++ src/com/cqupt/ui/widget/DynamicView.java
@@ -2,13 +2,23 @@ package com.cqupt.ui.widget;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.os.Bundle;
 import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
 
-public class DynamicView extends SurfaceView {
+public class DynamicView extends SurfaceView implements Callback{
+	private Bundle info;
+	
+	private SurfaceHolder mHolder;
 
-	public DynamicView(Context context) {
+	public DynamicView(Context context, Bundle b) {
 		this(context, null);
+		info = b;
+		mHolder = getHolder();
+		mHolder.addCallback(this);
 	}
 
 	public DynamicView(Context context, AttributeSet attrs) {
@@ -24,5 +34,8 @@ public class DynamicView extends SurfaceView {
 		super.onDraw(canvas);
 	}
 	
+	private void drawDynamicView(Canvas canvas, String value, Point poistion){
+		
+	}
 	
 }
