6f34b029c948160f1a1311d784ccbd2acd0a1d14
efa <yang_fandi@163.com>
2014-12-10 21:00:12 +0800

    BMHandler
    Signed-off-by: efa <yang_fandi@163.com>

--- src/com/cqupt/model/threaten/BMHandler.java
+++ src/com/cqupt/model/threaten/BMHandler.java
@@ -1,5 +1,6 @@
 package com.cqupt.model.threaten;
 
+import android.R.integer;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Message;
@@ -19,126 +20,134 @@ public class BMHandler extends Dispatcher.AbHandler {
 	public BMHandler() {
 		super(LEVEL);
 	}
+	
+	private static final int LEVEL1TIME=500;
+	private static final int LEVEL2TIME=200;
+	private static final int AUDIO=AudioManager.STREAM_ALARM;
 
 	@Override
 	public Bundle response(CanMsgInfo info) {
 		Bundle bd=new Bundle();
+		int alaLevel=info[2] % 4;
+		int dir=(info[6] >> 6) & 0x03;
+		
 		switch (((info[2] & 0xfc) >> 2)){
-		int arg1=data[2] % 4;
-		case 0x00: // rew
-			if (arg1 == 0x01) {
-				b.putInt("RID_1", R.drawable.rew);
-				b.putInt("RID", R.drawable.rew_1);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.YJTIME);
-				b.putInt("CJ", 0x10);
-			} else if (arg1 == 0x02) {
-				b.putInt("RID_1", R.drawable.rew);
-				b.putInt("RID", R.drawable.rew_2);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.EJTIME);
-				b.putInt("CJ", 0x20);
+		case 0x00: //rew
+			if (alaLevel == 0x01) {
+				
+				bundlePutInt(bd,R.drawable.rew,
+						R.drawable.rew_1,AUDIO,LEVEL1TIME);
+				
+			} else if (alaLevel == 0x02) {
+				
+				bundlePutInt(bd,R.drawable.rew,
+						R.drawable.rew_2,AUDIO,LEVEL2TIME);
+				
 			}
 			break;
 		case 0x01:// fcw
-			if (arg1 == 0x01) {
-				b.putInt("RID_1", R.drawable.fcw);
-				b.putInt("RID", R.drawable.fcw_1);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.YJTIME);
-				b.putInt("CJ", 0x11);
-			} else if (arg1 == 0x02) {
-				b.putInt("RID_1", R.drawable.fcw);
-				b.putInt("RID", R.drawable.fcw_2);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.EJTIME);
+			if (alaLevel == 0x01) {
+				
+				bundlePutInt(bd,R.drawable.fcw,
+						R.drawable.fcw_1,AUDIO,LEVEL1TIME);
+				
+			} else if (alaLevel == 0x02) {
+				
+				bundlePutInt(bd,R.drawable.fcw,
+						R.drawable.fcw_2,AUDIO,LEVEL2TIME);
+				
 			}
 			break;
 		case 0x02:// icw
-			if (arg1 == 0x01 && "left".equals(Dger_Dir)) {
-				b.putInt("RID_1", R.drawable.icw_left);
-				b.putInt("RID", R.drawable.icw_1_left);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.YJTIME);
-				b.putInt("CJ", 0x12);
-			} else if (arg1 == 0x01
-					&& "right".equals(Dger_Dir)) {
-				b.putInt("RID_1", R.drawable.icw_right);
-				b.putInt("RID", R.drawable.icw_1_right);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.YJTIME);
-				b.putInt("CJ", 0x22);
-			} else if (arg1 == 0x02
-					&& "left".equals(Dger_Dir)) {
-				b.putInt("RID_1", R.drawable.icw_left);
-				b.putInt("RID", R.drawable.icw_2_left);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.EJTIME);
-			} else if (msg2.arg1 == 0x02
-					&& "right".equals(Dger_Dir)) {
-				b.putInt("RID_1", R.drawable.icw_right);
-				b.putInt("RID", R.drawable.icw_2_right);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.EJTIME);
+			if (alaLevel == 0x01 && dir==0x01) {
+				
+				bundlePutInt(bd,R.drawable.icw_left,
+						R.drawable.icw_1_left,AUDIO,LEVEL1TIME);
+				
+			} else if (alaLevel == 0x01
+					&& dir==0x02) {
+				
+				bundlePutInt(bd,R.drawable.icw_right,
+						R.drawable.icw_1_right,AUDIO,LEVEL1TIME);
+				
+			} else if (alaLevel == 0x02
+					&& dir==0x01) {
+				
+				bundlePutInt(bd,R.drawable.icw_left,
+						R.drawable.icw_2_left,AUDIO,LEVEL2TIME);
+				
+			} else if (alaLevel == 0x02
+					&& dir==0x02) {
+				
+				bundlePutInt(bd,R.drawable.icw_right,
+						R.drawable.icw_2_right,AUDIO,LEVEL2TIME);
+				
 			}
 			break;
 		case 0x03:// cfcw
-			if (arg1 == 0x01) {
-				b.putInt("RID_1", R.drawable.cfcw);
-				b.putInt("RID", R.drawable.cfcw_1);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.YJTIME);
-			} else if (arg1 == 0x02) {
-				b.putInt("RID_1", R.drawable.cfcw);
-				b.putInt("RID", R.drawable.cfcw_2);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.EJTIME);
+			if (alaLevel == 0x01) {
+				
+				bundlePutInt(bd,R.drawable.cfcw,
+						R.drawable.cfcw_1,AUDIO,LEVEL1TIME);
+				
+			} else if (alaLevel == 0x02) {
+				
+				bundlePutInt(bd,R.drawable.cfcw,
+						R.drawable.cfcw_2,AUDIO,LEVEL2TIME);
+				
 			}
 			break;
 		case 0x05:// dnpw
-			if (arg1 == 0x01) {
-				b.putInt("RID_1", R.drawable.dnpw);
-				b.putInt("RID", R.drawable.dnpw_1);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.YJTIME);
-			} else if (msg5.arg1 == 0x02) {
-				b.putInt("RID_1", R.drawable.dnpw);
-				b.putInt("RID", R.drawable.dnpw_2);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.EJTIME);
+			if (alaLevel == 0x01) {
+				
+				bundlePutInt(bd,R.drawable.dnpw,
+						R.drawable.dnpw_1,AUDIO,LEVEL1TIME);
+				
+			} else if (alaLevel == 0x02) {
+
+				bundlePutInt(bd,R.drawable.dnpw,
+						R.drawable.dnpw_2,AUDIO,LEVEL2TIME);
+				
 			}
 			break;
 
 		case 0x06:// lcw
-			if (msg6.arg1 == 0x01 && "left".equals(Dger_Dir)) {
-				b.putInt("RID_1", R.drawable.lcw_left);
-				b.putInt("RID", R.drawable.lcw_1_left);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.YJTIME);
-			} else if (msg6.arg1 == 0x01
-					&& "right".equals(Dger_Dir)) {
-				b.putInt("RID_1", R.drawable.lcw_right);
-				b.putInt("RID", R.drawable.lcw_1_right);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.YJTIME);
-			} else if (msg6.arg1 == 0x02
-					&& "left".equals(Dger_Dir)) {
-				b.putInt("RID_1", R.drawable.lcw_left);
-				b.putInt("RID", R.drawable.lcw_2_left);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.EJTIME);
-			} else if (msg6.arg1 == 0x02
-					&& "right".equals(Dger_Dir)) {
-				b.putInt("RID_1", R.drawable.lcw_right);
-				b.putInt("RID", R.drawable.lcw_2_right);
-				b.putInt("RRID", AudioManager.STREAM_ALARM);
-				b.putInt("time", MainActivity.EJTIME);
+			if (alaLevel == 0x01 && dir==0x01) {
+
+				bundlePutInt(bd,R.drawable.lcw_left,
+						R.drawable.lcw_1_left,AUDIO,LEVEL1TIME);
+				
+			} else if (alaLevel == 0x01
+					&& dir==0x02) {
+
+				bundlePutInt(bd,R.drawable.lcw_right,
+						R.drawable.lcw_1_right,AUDIO,LEVEL1TIME);
+				
+			} else if (alaLevel== 0x02
+					&& dir==0x01) {
+
+				bundlePutInt(bd,R.drawable.lcw_left,
+						R.drawable.lcw_2_left,AUDIO,LEVEL2TIME);
+				
+			} else if (alaLevel == 0x02
+					&& dir==0x02) {
+
+				bundlePutInt(bd,R.drawable.lcw_right,
+						R.drawable.lcw_2_right,AUDIO,LEVEL2TIME);
+				
 			}
 			break;
 
 		}
-		
 		return bd;
+	}
+	
+	private Bundle bundlePutInt(Bundle bundle,int bmapID1,int bmapID2,int audio,int time) {
+		bundle.putInt("BMAP1", bmapID1);
+		bundle.putInt("BMAP2", bmapID2);
+		bundle.putInt("AUDIO", audio);
+		bundle.putInt("TIME", time);
+		return bundle;
 	}
 
 }
